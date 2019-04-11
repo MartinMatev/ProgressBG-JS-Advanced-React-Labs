1f2a096b9230762687995b6abbf952941d7578a8
WWWCourses <ivawebcourses@gmail.com>
2019-04-11 21:58:15 +0300

    Lab10: 11.04.2019 DONE: Regex till Alternation (slide 35) NEXT: next

--- lab10/word_boundary_demo.js
+++ lab10/word_boundary_demo.js
@@ -1,9 +1,14 @@
 // \w === [a-zA-Z0-9_]
 var arr_str = [
-  `mAdA`,  // Yes    
+  `AdA`,   // Yes     
+  `mAdA`,  // No    
+  `_AdA`,  // NO   
+  `-AdA`,  // Yes   
+  `!AdA!`,  // Yes   
+  `!AdA1`,  // No   
 ] ; 
 
-var re = /\bA.+A\b/;  
+var re = /\bAdA\b/;  
 
 // do the test:
 function regexTest(str){
