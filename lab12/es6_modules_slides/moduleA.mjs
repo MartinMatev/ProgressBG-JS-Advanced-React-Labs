6b83dea0a2c9894784f2e50556c0b8c6d21451f4
WWWCourses <ivawebcourses@gmail.com>
2019-04-18 22:33:17 +0300

    Lab12: 18.04.2019 DONE: member pattern NEXT: next TODO: check gitlog

--- lab12/es6_modules_slides/moduleA.mjs
+++ lab12/es6_modules_slides/moduleA.mjs
@@ -1,6 +1,12 @@
+export function aux(){
+  console.log(`I'm aux`);
+}
+
 function foo(){
+  aux();
   console.log(`foo from moduleA is called!`);
 }
+
 
 // exports a constant
 export const PI = 3.14;
